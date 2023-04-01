08ba639f3b1719586dd4973033cbeb90825740ac
unidagit <rhcjrdbsgml@gmail.com>
2023-04-01 20:39:33 +0900

    Feature : routes 생성

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Join from "../pages/JoinPage";
+import Home from "../template/Home";
+import LoginPage from "../pages/LoginPage";
 
-export default function Route() {
-  return <div>Route</div>;
+export default function IRoute() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/join" element={<Join />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
