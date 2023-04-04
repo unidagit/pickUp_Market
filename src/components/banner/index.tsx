f3bbe99af84719b83bc2f2c3988e53e042eb042b
unidagit <rhcjrdbsgml@gmail.com>
2023-04-04 11:15:44 +0900

    Feature : banner 컴포넌트 생성

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,7 @@
+import * as S from "./style";
+
+function Banner() {
+  return <S.BannerBox>Banner</S.BannerBox>;
+}
+
+export default Banner;
---
+++ src/components/banner/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const BannerBox = styled.div`
+  background-color: ${(props) => props.theme.sub.lighter};
+  height: 30vh;
+`;
