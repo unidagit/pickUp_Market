8cd92d3a1fae47510c24edecbfcae47d6a6ce721
unidagit <rhcjrdbsgml@gmail.com>
2023-04-04 11:24:53 +0900

    Feature : product route 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Join from "../pages/JoinPage";
-import Home from "../template/Home";
 import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import ProductDetailPage from "../pages/ProductDetailPage";
 
 export default function IRoute() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/join" element={<Join />}></Route>
+        <Route path="/products/:id" element={<ProductDetailPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
