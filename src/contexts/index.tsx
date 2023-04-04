28b4e3bd9d969e328e90a134247c258d9380bb3a
unidagit <rhcjrdbsgml@gmail.com>
2023-04-04 11:19:16 +0900

    Fix : type 이동

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,13 +1,10 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { ProviderProps } from "../common/types";
 import { queryClient } from "../services";
 import GlobalStyle from "../styles/global";
 import { theme } from "../styles/theme";
-
-type ProviderProps = {
-  children: React.ReactNode;
-};
 
 export default function AppProviders({ children }: ProviderProps) {
   return (
