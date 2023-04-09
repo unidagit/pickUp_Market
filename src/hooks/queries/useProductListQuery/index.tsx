cdca950573fe8c5728c2fa06544faf4a3d3c99d1
unidagit <rhcjrdbsgml@gmail.com>
2023-04-09 21:33:37 +0900

    Fix : type 수정

--- src/hooks/queries/useProductListQuery/index.tsx
+++ src/hooks/queries/useProductListQuery/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { apiGetProducts } from "../../../services/api";
-import { IProducts } from "../../../common/types/api";
+import { IProductBox } from "../../../common/types/api";
 
 export const useProductListQuery = () => {
   const {
@@ -8,7 +8,7 @@ export const useProductListQuery = () => {
     isLoading,
     isError,
     error,
-  } = useQuery<IProducts[]>(["products"], () => apiGetProducts());
+  } = useQuery<IProductBox[]>(["products"], () => apiGetProducts());
 
   return { productsList, isLoading, isError, error };
 };
