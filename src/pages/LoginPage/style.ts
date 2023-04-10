190abf9fd0dc97b41467edf7bc901edb9faf0990
unidagit <rhcjrdbsgml@gmail.com>
2023-04-10 23:24:59 +0900

    Desin : style 수정

--- src/pages/LoginPage/style.ts
+++ src/pages/LoginPage/style.ts
@@ -1,10 +1,20 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  width: 480px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 2rem;
+`;
+
 export const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   border: 1px solid ${(props) => props.theme.sub.lighter};
   border-radius: 0.5rem;
   padding: 35px;
-  width: 480px;
+  width: 100%;
 `;
 
 export const FormBox = styled.form``;
@@ -18,4 +28,9 @@ export const InputBox = styled.div`
 
 export const ErrorBox = styled.div`
   margin-bottom: 26px;
+`;
+
+export const JoinText = styled.p`
+  color: ${(props) => props.theme.sub.primary};
+  margin-top: 20px;
 `;
