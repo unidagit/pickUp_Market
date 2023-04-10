00e707e4646e5361475f846aaf078ea2310a1b2c
unidagit <rhcjrdbsgml@gmail.com>
2023-04-10 23:25:28 +0900

    Feat : joinPage 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Join from "../pages/JoinPage";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
 import ProductDetailPage from "../pages/Product/ProductDetailPage";
 import ProductSellerPage from "../pages/Product/ProductSellerPage";
 import ProductUploadPage from "../pages/Product/ProductUploadPage";
+import JoinPage from "../pages/JoinPage";
 
 export default function IRoute() {
   return (
@@ -13,6 +13,7 @@ export default function IRoute() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/join" element={<JoinPage />}></Route>
         <Route path="/products/:id" element={<ProductDetailPage />}></Route>
         <Route path="/seller" element={<ProductSellerPage />}></Route>
         <Route path="/seller/:type" element={<ProductUploadPage />}></Route>
