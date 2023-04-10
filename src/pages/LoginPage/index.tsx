19e3ad396de30da6b9a007aebae974c73e5b2c20
unidagit <rhcjrdbsgml@gmail.com>
2023-04-10 23:24:37 +0900

    Feat : 회원가입 link 추가

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -7,6 +7,7 @@ import { ErrorText } from "../../components/text";
 import { useLoginMutation } from "../../hooks/mutations/useLoginQuery";
 import Button from "../../components/atoms/buttons";
 import Label from "../../components/atoms/label";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
   const {
@@ -25,41 +26,46 @@ function LoginPage() {
 
   return (
     <CenterLayout>
-      <S.LoginWrapper>
-        <S.FormBox onSubmit={handleSubmit(onSubmit)}>
-          <S.InputBox>
-            <Label htmlFor="username">
-              <LoginInput
-                id="username"
-                type="username"
-                placeholder="아이디"
-                register={register("username", {
-                  required: "* 필수 입력사항입니다.",
-                })}
-                errors={errors.username}
-              />
-            </Label>
+      <S.Wrapper>
+        <S.LoginWrapper>
+          <S.FormBox onSubmit={handleSubmit(onSubmit)}>
+            <S.InputBox>
+              <Label htmlFor="username">
+                <LoginInput
+                  id="username"
+                  type="username"
+                  placeholder="아이디"
+                  register={register("username", {
+                    required: "* 필수 입력사항입니다.",
+                  })}
+                  errors={errors.username}
+                />
+              </Label>
 
-            <Label htmlFor="password">
-              <LoginInput
-                id="password"
-                type="password"
-                placeholder="비밀번호"
-                register={register("password", {
-                  required: "* 필수 입력사항입니다.",
-                })}
-                errors={errors.password}
-              />
-            </Label>
-          </S.InputBox>
-          <S.ErrorBox>
-            <ErrorText>{errorMessage}</ErrorText>
-          </S.ErrorBox>
-          <Button disabled={!isValid} type="submit">
-            로그인
-          </Button>
-        </S.FormBox>
-      </S.LoginWrapper>
+              <Label htmlFor="password">
+                <LoginInput
+                  id="password"
+                  type="password"
+                  placeholder="비밀번호"
+                  register={register("password", {
+                    required: "* 필수 입력사항입니다.",
+                  })}
+                  errors={errors.password}
+                />
+              </Label>
+            </S.InputBox>
+            <S.ErrorBox>
+              <ErrorText>{errorMessage}</ErrorText>
+            </S.ErrorBox>
+            <Button disabled={!isValid} type="submit">
+              로그인
+            </Button>
+          </S.FormBox>
+        </S.LoginWrapper>
+        <S.JoinText>
+          <Link to="/join">회원가입</Link>
+        </S.JoinText>
+      </S.Wrapper>
     </CenterLayout>
   );
 }
