99291ee4bdb7963d44409a4101ba22dd4ba2a59c
unidagit <rhcjrdbsgml@gmail.com>
2023-04-13 11:02:19 +0900

    Fix : 로딩처리 코드 정리

--- src/hooks/queries/useProductListQuery/index.tsx
+++ src/hooks/queries/useProductListQuery/index.tsx
@@ -1,16 +1,13 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { apiGetProducts } from "../../../services/api";
-import { IProducts } from "../../../common/types/api";
 
 export const useProductListQuery = () => {
   const {
-    data,
-    isLoading,
+    data, //쿼리에서 가져온 현재 데이터를 보유합니다.
     fetchNextPage, //function 다음페이지를 가져오는 함수, 페이지 맨 아래로 스크롤할 때마다 이 함수를 호출해야합니다.
-    hasNextPage,
-    isFetchingNextPage, //boolean 다음페이지를 가져올때 확인합니다.
-    status,
-    error,
+    hasNextPage, //가져올 데이터가 더 있는지 여부를 나타내는 boolean
+    isFetchingNextPage, //현재 다음 페이지의 데이터를 가져오는 중인지 여부를 나타내는 boolean
+    isFetching, //현재 데이터를 가져오고 있는지 여부를 나타내는 boolean
   } = useInfiniteQuery(
     ["products"],
     ({ pageParam = 1 }) => apiGetProducts(pageParam),
@@ -18,22 +15,17 @@ export const useProductListQuery = () => {
     {
       getNextPageParam: (lastPage, allPages) => {
         //스크롤감지 후 다음페이지 불러오는 아이.
-        console.log(allPages.length);
         const nextPage = allPages.length + 1;
         return nextPage; //여기서 리턴을 하면 바로 apiGetProducts인자로 간다.
-        // return lastPage.next || undefined;
       },
     }
   );
-  console.log(data);
 
   return {
     data,
-    isLoading,
+    isFetching,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
-    status,
-    error,
   };
 };
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -6,39 +6,40 @@ import ProductCard from "../../components/contents/productCard";
 import * as S from "./style";
 import Footer from "../../components/footer";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { IProductResult } from "../../common/types/api";
 
 function HomePage() {
-  //1.리스트 불러오기
-  //2.리스트 카드 컴포넌트 만들어서 map으로 돌리기
-  //3. 리스트 카드 누르면 디테일 페이지로 이동
-
-  const { data, isLoading, fetchNextPage, hasNextPage } = useProductListQuery();
-
-  if (isLoading) {
-    return <div className="loading">Loading</div>;
-  }
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching } =
+    useProductListQuery();
 
   return (
     <>
       <DefaultLayout>
         <Banner />
-        <S.ProductListWrapper>
-          <InfiniteScroll
-            dataLength={data?.pages?.length || 0}
-            next={fetchNextPage}
-            hasMore={!!hasNextPage}
-            loader={<Spinner />}
-          >
+        <InfiniteScroll
+          dataLength={data?.pages?.length || 0}
+          next={fetchNextPage}
+          hasMore={!!hasNextPage}
+          loader={
+            isFetchingNextPage && hasNextPage ? (
+              <Spinner />
+            ) : (
+              "마지막 상품입니다."
+            )
+          }
+        >
+          <S.ProductListWrapper>
             {data &&
               data.pages.map((el) =>
-                el.results.map((item: any) => (
+                el.results.map((item: IProductResult) => (
                   <S.ProductListBox key={item.product_id}>
                     <ProductCard key={item.product_id} {...item} />
                   </S.ProductListBox>
                 ))
               )}
-          </InfiniteScroll>
-        </S.ProductListWrapper>
+            <div>{isFetching && !isFetchingNextPage ? <Spinner /> : null}</div>
+          </S.ProductListWrapper>
+        </InfiniteScroll>
       </DefaultLayout>
       <Footer />
     </>
