012de555750266efe56bee5f32334448f51b06da
unidagit <rhcjrdbsgml@gmail.com>
2023-04-13 15:56:22 +0900

    Fix : option delete

--- src/services/query-client/index.tsx
+++ src/services/query-client/index.tsx
@@ -1,10 +1,3 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: true, // stale 상태일 경우 윈도우 포커싱 될 때 마다 refetch
-      refetchOnMount: false, //  stale 상태일 경우 마운트 시 마다 refetch
-    },
-  },
-});
+export const queryClient = new QueryClient();
