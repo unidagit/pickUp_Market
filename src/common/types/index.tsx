732b44674d5c52de51be814f32ac39aebb89db31
unidagit <rhcjrdbsgml@gmail.com>
2023-04-14 23:29:03 +0900

    Feat : ICartList type 추가

--- src/common/types/index.tsx
+++ src/common/types/index.tsx
@@ -56,3 +56,11 @@ export interface ICountProps {
   userType: string | null;
   stock: number;
 }
+
+export interface ICartList {
+  cart_item_id: number;
+  is_active: boolean;
+  my_cart: number;
+  product_id: number;
+  quantity: number;
+}
