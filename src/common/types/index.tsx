20c7f9e6c21f1ebd76c52198f52a105ed4ef4e77
unidagit <rhcjrdbsgml@gmail.com>
2023-04-18 20:47:07 +0900

    Feat : ICartListData 추가

--- src/common/types/index.tsx
+++ src/common/types/index.tsx
@@ -53,9 +53,21 @@ export interface SelectTabProps {
 
 export interface ICountProps {
   count: number;
-  setCount: React.Dispatch<React.SetStateAction<number>>;
-  userType: string | null;
+  setCount?: React.Dispatch<React.SetStateAction<number>>;
+  userType?: string | null;
   stock: number;
+  handleMinus?: any;
+  handleAdd?: any;
+}
+
+export interface ICartListData {
+  data: {
+    cart_item_id: number;
+    is_active: boolean;
+    my_cart: number;
+    product_id: number;
+    quantity: number;
+  };
 }
 
 export interface ICartList {
