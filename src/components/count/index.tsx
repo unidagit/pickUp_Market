d49cb9be5cc048310eb7d5f562d45fe1a966e625
unidagit <rhcjrdbsgml@gmail.com>
2023-04-24 14:28:20 +0900

    Fix : count props 수정

--- src/components/count/index.tsx
+++ src/components/count/index.tsx
@@ -1,39 +1,19 @@
 import { ICountProps } from "../../common/types";
 import * as S from "./style";
 
-function Count({ count, setCount, userType, stock }: ICountProps) {
-  const handleMinus = () => {
-    if (userType === "SELLER") {
-      alert("판매자는 구매할 수 없습니다.");
-      return;
-    }
-
-    if (count > 1) {
-      setCount(count - 1);
-      return;
-    }
-  };
-
-  const handleAdd = () => {
-    //seller 금지
-    if (userType === "SELLER") {
-      alert("판매자는 구매할 수 없습니다.");
-      return;
-    }
-
-    if (stock > count && stock > 0) {
-      setCount(count + 1);
-      return;
-    } else if (stock === 0) {
-      alert("현재 품절입니다");
-    }
-  };
-
+function Count({
+  count,
+  // setCount,
+  // userType,
+  stock,
+  handleMinus,
+  handleAdd,
+}: ICountProps) {
   return (
     <S.CountBox>
-      <S.MinusButton onClick={handleMinus}>-</S.MinusButton>
+      <S.MinusButton onClick={() => handleMinus()}>-</S.MinusButton>
       <S.Num>{count}</S.Num>
-      <S.AddButton onClick={handleAdd}>+</S.AddButton>
+      <S.AddButton onClick={() => handleAdd(stock)}>+</S.AddButton>
     </S.CountBox>
   );
 }
