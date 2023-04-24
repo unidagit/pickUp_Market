ab237b29f391696d8f6058e7b9125be2f1c5b510
unidagit <rhcjrdbsgml@gmail.com>
2023-04-24 14:25:22 +0900

    Fix : retry 추가

--- src/hooks/queries/useProductListQuery/index.tsx
+++ src/hooks/queries/useProductListQuery/index.tsx
@@ -7,7 +7,6 @@ export const useProductListQuery = () => {
     fetchNextPage, //function 다음페이지를 가져오는 함수, 페이지 맨 아래로 스크롤할 때마다 이 함수를 호출해야합니다.
     hasNextPage, //가져올 데이터가 더 있는지 여부를 나타내는 boolean
     isFetchingNextPage, //현재 다음 페이지의 데이터를 가져오는 중인지 여부를 나타내는 boolean
-    isFetching, //현재 데이터를 가져오고 있는지 여부를 나타내는 boolean
   } = useInfiniteQuery(
     ["products"],
     ({ pageParam = 1 }) => apiGetProducts(pageParam),
@@ -18,12 +17,12 @@ export const useProductListQuery = () => {
         const nextPage = allPages.length + 1;
         return nextPage; //여기서 리턴을 하면 바로 apiGetProducts인자로 간다.
       },
+      retry: 0,
     }
   );
 
   return {
     data,
-    isFetching,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
