edb84b0d48a313fb7e7aa757e0b26f365b69ebf9
unidagit <rhcjrdbsgml@gmail.com>
2023-04-24 10:57:30 +0900

    Feat : footer 컴포넌트 구현

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,25 @@
 import * as S from "./style";
 
 function Footer() {
-  return <S.FooterWrapper>Footer</S.FooterWrapper>;
+  return (
+    <S.FooterWrapper>
+      <S.FooterInfo>
+        <S.FooterList>
+          <S.Text>픽업마켓 소개</S.Text>
+          <S.Text>이용약관</S.Text>
+          <S.Text>개인정보처리방침</S.Text>
+          <S.Text>전자금융거래약관</S.Text>
+          <S.Text>청소년보호정책</S.Text>
+        </S.FooterList>
+        <S.CompanyInfo>
+          <S.Text>(주)pickUp Market</S.Text>
+          <S.Text>서울특별시 강남구 125-56</S.Text>
+          <S.Text>사업자 번호: 000-0000-0000</S.Text>
+          <S.Text>대표: 픽업</S.Text>
+        </S.CompanyInfo>
+      </S.FooterInfo>
+    </S.FooterWrapper>
+  );
 }
 
 export default Footer;
--- src/components/footer/style.ts
+++ src/components/footer/style.ts
@@ -1,6 +1,35 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.footer`
-  background-color: white;
-  height: 298px;
+  background-color: ${(props) => props.theme.sub.verylighter};
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const FooterInfo = styled.table`
+  display: flex;
+  flex-direction: column;
+  font-size: 13px;
+  max-width: 1280px;
+  margin: 0 auto;
+  color: ${(props) => props.theme.sub.primary};
+  width: 1280px;
+`;
+
+export const FooterList = styled.td`
+  display: flex;
+  column-gap: 2.4rem;
+`;
+
+export const CompanyInfo = styled.td`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+export const Text = styled.tr`
+  margin-bottom: 5px;
 `;
