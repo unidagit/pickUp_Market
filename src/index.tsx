1da48a96f04ebcc18ef67cf2693ebdf0c894bfbc
unidagit <rhcjrdbsgml@gmail.com>
2023-04-24 23:20:12 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <AppProviders>
+    <App />
+  </AppProviders>
+  // </React.StrictMode>
 );
